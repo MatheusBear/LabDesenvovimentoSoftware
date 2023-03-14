a7d97a36369ca6fd0ef3cb49ed183c7b4ba742b6
Matheus-Belo <mbmelo1421@gmail.com>
2023-03-14 12:21:18 -0300

    menus no main e turmas

--- Implementação/backend/Lab1_ProjetoSoftware/src/main/service/Impl/TurmaServiceImpl.java
+++ Implementação/backend/Lab1_ProjetoSoftware/src/main/service/Impl/TurmaServiceImpl.java
@@ -3,25 +3,79 @@ package main.service.Impl;
 import main.model.Turma;
 import main.service.TurmaService;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 public class TurmaServiceImpl implements TurmaService {
     @Override
     public Turma criarTurma(Turma turma) {
-        return null;
+        Date dataAtual = new Date();
+
+        Turma turmaAtual = findTurmaByDocumentoLegal(turma.getDocumentoLegal());
+        if ( turmaAtual == null){
+
+            Long id= geradorDeId();
+            turmaAtual = new Turma(
+            id,
+            id,
+            turma.getAlunos(),
+            turma.getCurso(),
+            turma.getProfessor(),
+
+            ""+dataAtual.getYear(),
+            null,
+            turma.getSala(),
+            );
+            escreverTurmaNoArquivo(turmaAtual,"Turmas.txt");
+        }
+        return turmaAtual;
+    }
+
+    private Long geradorDeId() throws Exception {
+
+        LinkedList<Turma> turmasMatriculadas = (LinkedList<Turma>) carregarArquivoTurmaParaMemoria("Turmas.txt");
+
+        if(turmasMatriculadas.size() <= 0) {
+            return 1L;
+        }else{
+            Turma turma = turmasMatriculadas.get(turmasMatriculadas.size()-1);
+            return (turma.getId()+1L);
+        }
     }
 
     @Override
-    public Turma editarTurma(Turma turma) {
-        return null;
+    public Turma editarTurma(Long id, Turma turmaModificada) {
+        Turma turma = findTurmaById(id);
+        if ( turma!= null){
+            deletarTurma(id);
+            turma = turmaModificada.clone();
+            escreverTurmaNoArquivo(turma,"Turmas.txt");
+        }else{
+            System.out.println("TURMA NÃO ENCONTRADO");
+        }
+        return turma;
     }
 
     @Override
-    public Turma deletarTurma(Turma turma) {
-        return null;
+    public Turma deletarTurma(Long id) {
+        Turma turma = findTurmaById(id);
+        if ( turma!= null){
+
+            excluirTurmaNoArquivo(turma,"Turmas.txt");
+        }else{
+            System.out.println("TURMA NÃO ENCONTRADO");
+        }
+        return turma;
     }
 
     @Override
     public Turma getTurma(Long idDisciplina, String numeroDisciplina) {
         return null;
+
     }
 
     @Override
--- Implementação/backend/Lab1_ProjetoSoftware/src/main/service/TurmaService.java
+++ Implementação/backend/Lab1_ProjetoSoftware/src/main/service/TurmaService.java
@@ -4,12 +4,16 @@ import main.model.Turma;
 
 public interface TurmaService {
 
-
+//Turma turma
     Turma criarTurma(Turma turma);
-    Turma editarTurma(Turma turma);
-    Turma deletarTurma(Turma turma);
+    //Turma turma - throws Exception
+    Turma editarTurma(Long id, Turma turmaModificada)throws Exception;
+    //Turma turma - throws Exception
+    Turma deletarTurma(Long id) throws Exception;
 
     Turma getTurma(Long idDisciplina, String numeroDisciplina);
 
     boolean matricularAluno(Long id, Turma turma);
+    //criei
+    Turma findTurmaById(Long idTurma) throws Exception;
 }
